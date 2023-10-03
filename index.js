3194723818248df333bc75a5633134c55789c72a
V Avinash <97448261+avinashvishu@users.noreply.github.com>
2023-10-03 18:17:27 +0530

    btn text of X->*

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ for (item of buttons) {
         console.log('Button text is ', buttonText);
         if (buttonText == 'X') {
             buttonText = '*';
+            console.log(buttonText);
             screenValue += buttonText;
             screen.value = screenValue;
         } else if (buttonText == 'C') {
